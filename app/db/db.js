57f453ed1adffafc796ac2efcc608ab6aee3bbf2
Leonard LiuTao <364125168@qq.com>
2016-08-27 20:20:39 +0800

    db use mLab

--- app/db/db.js
+++ app/db/db.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 
 module.exports = {
   connect: function (mode, callback) {
-    let url = 'mongodb://localhost/freetravel-db';
+    // let url = 'mongodb://localhost/freetravel-db';
+    // let url = 'mongodb://<Leonard>:<liutao19950414>@ds017726.mlab.com:17726/freetravel-db';
+    let url = 'mongodb://Leonard:liutao19950414@ds017736.mlab.com:17736/freetravel-db';
     if (mode === 'test') {
       url = 'mongodb://localhost/freetravel-test-db';
     }
